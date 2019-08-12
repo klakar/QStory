6760e2d54389e9ce9d17bbd6647148d7ab5b8b7d
Klas Karlsson <klaskarlsson@hotmail.com>
2019-08-12 17:56:27 +0200

    Working Protorype

--- qstory/qstory.py
+++ qstory/qstory.py
@@ -32,6 +32,7 @@ from .qstory_dockwidget import QStoryDockWidget
 import os.path
 from math import log, exp
 import json
+from distutils.dir_util import copy_tree
 
 # Import functions from QGIS
 from qgis.utils import iface
@@ -46,6 +47,7 @@ qstory_location = []        # Map location lon,lat string for the story page
 qstory_zoom = []            # Map zoom level integer for the story page
 current_index = 0           # Currently active page index in the widget
 themes_dir = []             # Folder in the plugin dir for story themes
+first_run = True            # Some things should only run once on start...
 
 #------------------------------------------------------------------------
 
@@ -251,7 +253,10 @@ class QStory:
         self.get_themes()
 
         # Initial Preview "help" text
-        help_text = '<h3>QStory</h3><p>Here there will be some simple "Get Started" text...'
+        help_text = '<h3>QStory</h3><p>Here there will be some simple "Get Started" text...</p>'
+        # Credits
+        help_text += '<div style="font-size: 10pt;text-decoration: underline; font-weight: bold;">Credits</div>'
+        help_text += '<div style="font-size: 8pt;">Icons made by <a href="https://www.flaticon.com/authors/google" title="Google">Google</a> from <a href="https://www.flaticon.com/"             title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"             title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>'
         self.dockwidget.web_view.setHtml(help_text)
 
         # Set first story page
@@ -264,7 +269,8 @@ class QStory:
         qstory_zoom.insert(0, 4)
 
         # Update the widget
-        self.update_page(0)
+        if not first_run:
+            self.update_page(0)
 
         
 
@@ -273,6 +279,7 @@ class QStory:
 
     # Get all the themes from the themes folder (in the plugin directory)
     def get_themes(self):
+        self.dockwidget.cmb_theme.clear()
         for f in os.listdir(themes_dir):
             self.dockwidget.cmb_theme.addItem(f)
 
@@ -322,12 +329,24 @@ class QStory:
         # Insert the page in the lists
         qstory_header.insert(new_page, 'New Title')
         qstory_body.insert(new_page, 'New Content')
-        qstory_location.insert(new_page, '0,0')
-        qstory_zoom.insert(new_page, 6)
+        ccX, ccY, ccZ = self.canvas_center()
+        qstory_location.insert(new_page, '%s,%s' % (str(ccX), str(ccY)))
+        qstory_zoom.insert(new_page, ccZ)
 
         # Show the new page
         self.update_page(new_page)
         
+    # Canvas center in lon,lat,zoom
+    def canvas_center(self):
+        center = iface.mapCanvas().center()
+        canvasCrs = iface.mapCanvas().mapSettings().destinationCrs()
+        qstoryCrs = QgsCoordinateReferenceSystem(4326)
+        transform = QgsCoordinateTransform(canvasCrs, qstoryCrs, QgsProject.instance())
+        canvas_X, canvas_Y = transform.transform(center.x(), center.y())
+        canvas_scale = iface.mapCanvas().scale()
+        canvas_zoom = round(log(591657550.500000 /(canvas_scale/2))/log(2))
+        return canvas_X, canvas_Y, canvas_zoom
+
 
     # Update the current page with the list content for selected page number
     def update_page(self, page_index):
@@ -347,7 +366,6 @@ class QStory:
         self.dockwidget.cmb_page.setCurrentIndex(page_index)
         self.dockwidget.spin_zoom.setValue(qstory_zoom[page_index])
         current_index = page_index
-        self.dockwidget.cmb_page.setCurrentIndex(page_index)
         
         # Update the map canvas to the current location and zoom level
         self.set_center()
@@ -373,13 +391,6 @@ class QStory:
         
         # Create a local variable to hold the generated JS string
         export_content = [qstory_header, qstory_body, qstory_location, qstory_zoom]
-        #export_story = 'story_title = %s;\n' % (str(qstory_header))
-        #export_story += 'story_content = %s;\n' % (str(qstory_body))
-        #export_story += 'story_location = ['        # Break apart the strings in location to a new list of float lists. I.E. [[0,0], [0,0]]
-        #for single_location in qstory_location:
-        #    export_story += '%s, ' % (str([float(i) for i in single_location.split(',')]))
-        #export_story = export_story[:-2] + '];\n'    # Remove last comma and blank space from loop above
-        #export_story += 'story_zoom = %s;\n' % (str(qstory_zoom))
         return export_content
 
     # Convert coordinate from srsCrs to dstCrs
@@ -393,8 +404,37 @@ class QStory:
         iface.mapCanvas().setCenter(QgsPointXY(canvas_X, canvas_Y))
         canvas_scale = (591657550.500000 * 2) / (exp(log(2) * int(self.dockwidget.spin_zoom.value())))
         iface.mapCanvas().zoomScale(canvas_scale)
-        
 
+    # Copy Theme folder to web site folder
+    def copy_2_web(self):
+        # Create the paths to the web and selected themes dir and copy the content from themes to web (creating the qstory folder)
+        web_dir = os.path.join(os.path.dirname(self.dockwidget.html_file.filePath()), 'qstory')
+        selected_theme = os.path.join(themes_dir, self.dockwidget.cmb_theme.currentText())
+        print(web_dir)
+        print(selected_theme)
+        copy_tree(selected_theme, web_dir)
+
+    # Generate the story JS and save it to "pages.js" in the web folder (in the qstory folder)
+    def generate_storyjs(self):
+        # Generate the JS content
+        js_content = 'var story_title = %s;\n' % (str(qstory_header))
+        js_content += 'var story_content = %s;\n' % (str(qstory_body))
+        js_content += 'var story_location = ['        # Break apart the strings in location to a new list of float lists. I.E. [[0,0], [0,0]]
+        for single_location in qstory_location:
+            js_content += '%s, ' % (str([float(i) for i in single_location.split(',')]))
+        js_content = js_content[:-2] + '];\n'    # Remove last comma and blank space from loop above
+        js_content += 'var story_zoom = %s;\n' % (str(qstory_zoom))
+        # Get the web qstory path
+        web_dir = os.path.join(os.path.dirname(self.dockwidget.html_file.filePath()), 'qstory')
+        try:
+            os.mkdir(web_dir)
+        except:
+            print('qstory directory already exist...')
+        # Create the pages.js file and write the content to it
+        pages_file = open(os.path.join(web_dir, 'pages.js'), 'w')
+        pages_file.write(js_content)
+        pages_file.close()
+        
 
     #--------------------------------------------------------------------------
     # Here are most "first call" functions for the QStory dock widgetfrom qgis.utils import iface
@@ -404,8 +444,31 @@ class QStory:
         # Start by updating all and move to the first page
         self.update_page(0)
 
-        # Do something with the content... (to be updated)
-        self.dockwidget.txt_body.setPlainText(str(self.generate_output()))
+        # Test if the selected file is valid
+        # Copy Themes folder to web map folder under name "qstory"
+        self.copy_2_web()
+        # Generate pages.js file in the new folder
+        self.generate_storyjs()
+        # Add reference to css and JS in the web map start file
+        style_add = '<link rel="stylesheet" href="./qstory/qstory.css"></head>'
+        js_add = '<div id="story"><div id="story_header">Title</div><div id="story_body"></div><div id="story_footer"></div></div><script src="./qstory/pages.js"></script><script src="./qstory/qstory.js"></script></body>'
+        # Read the web file
+        web_file = open(self.dockwidget.html_file.filePath(), 'r')
+        web_content = web_file.read()
+        web_file.close()
+        print(web_content)
+        if web_content.find(style_add):     # If the link to qstory.css exist, do nothing. Otherwise continue...
+            web_content = web_content.replace('</head>', style_add)
+            web_content = web_content.replace('</body>', js_add)
+            # Save the new content to the qstory.htm copy of the web file
+            story_file = open(os.path.join(os.path.dirname(self.dockwidget.html_file.filePath()), 'qstory.htm'),'w')
+            story_file.write(web_content)
+            story_file.close()
+        """
+        The above if statement will not create a new htm file if the file used as a source already have
+        the links and placeholders in it. If it have a slightly different string, the new ones will be added anyway.
+        If the filename for the source is the same (qstory.htm) the old file will be overritten with the new content.
+        """
 
 
     def story_content_tab(self):    # Check if the second (preview) tab is active, and in that case update preview of the story page
@@ -429,15 +492,9 @@ class QStory:
         self.update_page(current_index - 1)
 
     def get_center(self):           #Get the center coordinate from the canvas, transform it to wgs-84 and estimate zoom level from scale.
-        center = iface.mapCanvas().center()
-        canvasCrs = iface.mapCanvas().mapSettings().destinationCrs()
-        qstoryCrs = QgsCoordinateReferenceSystem(4326)
-        transform = QgsCoordinateTransform(canvasCrs, qstoryCrs, QgsProject.instance())
-        canvas_X, canvas_Y = transform.transform(center.x(), center.y())
-        self.dockwidget.txt_center.setText(str(round(canvas_X,3)) + ',' + str(round(canvas_Y,3)))
-        canvas_scale = iface.mapCanvas().scale()
-        canvas_zoom = round(log(591657550.500000 /(canvas_scale/2))/log(2))
-        self.dockwidget.spin_zoom.setValue(canvas_zoom)
+        ccX, ccY, ccZ = self.canvas_center()
+        self.dockwidget.txt_center.setText('%s,%s' % (str(round(ccX,4)), str(round(ccY,4))))
+        self.dockwidget.spin_zoom.setValue(ccZ)
 
     def save_story(self):
         # Create file
